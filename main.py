12555c095c1e9de69f170a2e1e130823dd9687f4
The Professor <brandonlee2013@gmail.com>
2022-07-31 08:49:56 -0400

    commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import discord
 import time
+import os
+import json
 
 """
 This BOT file will have to run client.run(token) at all times for Bot to be active
@@ -17,6 +19,12 @@ def read_token():
         lines = f.readlines()
         return lines[0].strip()
 
+
+def get_quote():
+    response = requests.get("http://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return quote
 
 token = read_token()
 client = discord.Client()
@@ -42,13 +50,17 @@ async def on_message(message):
 
     if message.content == "!help":
         embed = discord.Embed(title="Help on BOT", description="Here are some useful commands. These only work in channel: general_dev")
-        embed.add_field(name="!hello bot", value="Bot says Hi")
+        embed.add_field(name="!hello", value="Bot says Hi")
+        embed.add_field(name="!inspire", value="Bot will give you an inspiring quote")
         await message.channel.send(content=None, embed=embed)
 
     if message.channel.name == 'general_dev':
-
-        if message.content == "!hello bot":
+        if message.content == "!hello":
             await message.channel.send(f"Hi there, {username}")
+        if message.content == "!inspire":
+            quote = get_quote()
+            await message.channel.send(quote)
+
 
 
 """Un-comment client.run(token) and run script for bot to activate"""
